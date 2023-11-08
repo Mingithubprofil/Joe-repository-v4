01093d6ce87dacc8db22ca71351d338dd1b3657e
Mingithubprofil <sikj21ad@student.cbs.dk>
2023-11-08 16:47:03 +0100

    files

--- client/scripts/login.js
+++ client/scripts/login.js
@@ -29,7 +29,7 @@ function loginUser() {
       responseDOM.innerHTML = response.data;
 
       await wait(3)
-      location.href = "/juicechat.html";
+      location.href = "/path/to/userHome.html";
     })
     .catch(function (error) {
       console.log(error);
