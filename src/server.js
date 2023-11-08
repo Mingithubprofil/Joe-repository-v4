0744779307bab5fab0afbb03e1bc9208350fd2c3
Mingithubprofil <sikj21ad@student.cbs.dk>
2023-11-08 17:48:07 +0100

    files

--- src/server.js
+++ src/server.js
@@ -25,14 +25,14 @@ app.get('/home', (req, res) => {
 });
 
 
-app.get('/userHome', (req, res) => {
+/*app.get('/userHome', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/pages/userHome.html'));
 });
 
 app.get('/juicechat', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/pages/juicechat.html'));
   
-}); 
+}); */
 
 app.get('/juicechat.js', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/scripts/juicechat.js'));
