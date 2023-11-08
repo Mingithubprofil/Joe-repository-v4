7f86185957499c5a3518929fd9e9c5fb892aa5dc
Mingithubprofil <sikj21ad@student.cbs.dk>
2023-11-08 16:50:59 +0100

    files

--- client/scripts/login.js
+++ client/scripts/login.js
@@ -29,7 +29,7 @@ function loginUser() {
       responseDOM.innerHTML = response.data;
 
       await wait(3)
-      location.href = "/path/to/userHome.html";
+      location.href = "userHome.html";
     })
     .catch(function (error) {
       console.log(error);
