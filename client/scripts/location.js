8b76513408cdbf41d9480669d7706f08ff337d15
Mingithubprofil <sikj21ad@student.cbs.dk>
2023-11-10 22:52:21 +0100

    weather api

--- client/scripts/location.js
+++ client/scripts/location.js
@@ -11,7 +11,7 @@ async function getWeather() {
     setTimeout(async () => {
       try {
         const result = await axios.get(
-          "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m"
+            "https://api.open-meteo.com/v1/forecast?latitude=55.6759&longitude=12.5655&hourly=temperature_2m"
         );
         resolve(result);
       } catch (err) {
