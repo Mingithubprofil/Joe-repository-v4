60a8f8d1ac960faaae1c2882d9f7f1ffb0e50c55
Mingithubprofil <sikj21ad@student.cbs.dk>
2023-11-12 00:56:36 +0100

    files

---
+++ src/db/db.js
@@ -0,0 +1,46 @@
+
+
+const sql = require('mssql');
+
+const config = {
+  user: 'admin20',
+  password: 'Kodetdatabase20',
+  server: 'servertilhjemmeside.database.windows.net',
+  database: 'hjemmesidedatabase',
+  options: {
+    encrypt: true, // Sørg for at aktivere kryptering
+  },
+};
+
+sql.connect(config)
+  .then(pool => {
+    console.log('Forbundet til Azure SQL Database');
+
+    // Udfør SQL-operationer her (indsæt, opdater, hent, slet data)
+    return pool.request().query('SELECT * FROM Users');
+  })
+  .then(result => {
+    console.dir(result.recordset);
+  })
+  .catch(err => console.error('Fejl ved forbindelse til Azure SQL Database:', err))
+  .finally(() => {
+    // Luk forbindelsen ved afslutning af din applikation
+    sql.close();
+    console.log('Forbindelse til Azure SQL Database lukket ved afslutning.');
+  });
+
+  
+/* 
+
+//Luk forbindelsen ved afslutning af din applikation
+
+process.on('SIGINT', () => {
+  sql.close();
+  console.log('Forbindelse til Azure SQL Database lukket ved afslutning.');
+  process.exit();
+});
+
+*/
+
+
+
