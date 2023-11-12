6f9d0dd4c42b06865c0a55453c9723fa790020e3
Mingithubprofil <sikj21ad@student.cbs.dk>
2023-11-12 14:28:40 +0100

    database

--- src/db/db.js
+++ src/db/db.js
@@ -1,62 +1,11 @@
 
-/*
-//configuration af db
 
-const sql = require('mssql');
-
-const config = {
-  user: 'admin20',
-  password: 'Kodetdatabase20',
-  server: 'servertilhjemmeside.database.windows.net',
-  database: 'hjemmesidedatabase',
-  options: {
-    encrypt: true, 
-  },
-};
-
-//connection til db
-
-async function connectAndQuery() {
-    try {
-      // Opretter forbindelse til databasen
-      const pool = await sql.connect(config);
-      console.log('Forbundet til Azure SQL Database');
-  
-      // Opretter en forespørgsel for tjek
-      const result = await pool.request().query('SELECT * FROM Users');
-      
-      // Logger resultatet af forespørgslen
-      console.log('Data fra databasen:', result.recordset);
-    } catch (err) {
-      // Håndterer fejl ved forbindelse eller forespørgsel
-      console.error('Fejl ved forbindelse til Azure SQL Database:', err.message);
-      console.error('Fejldetaljer:', err);
-    } finally {
-      // Lukker forbindelsen ved afslutning af applikationen
-      sql.close();
-      console.log('Forbindelse til Azure SQL Database lukket ved afslutning.');
-    }
-  }
-  
-  // Til at køre funktionen
-
-  connectAndQuery();
-  
-
-/* 
-
-//Til lukning af forbindelsen ved afslutning af applikationen
-
-process.on('SIGINT', () => {
-  sql.close();
-  console.log('Forbindelse til Azure SQL Database lukket ved afslutning.');
-  process.exit();
-});
-
-*/
+//database
 
 const { Connection, Request } = require('tedious');
 
+
+// Konfigurationsoplysninger for forbindelsen 
 const config = {
   server: 'servertilhjemmeside.database.windows.net',
   authentication: {
@@ -67,16 +16,21 @@ const config = {
     },
   },
   options: {
-    encrypt: true, // Sørg for at aktivere kryptering
+    encrypt: true, // Aktiver kryptering
     database: 'hjemmesidedatabase',
+    trustServerCertificate: false, // Brug kun i udviklingsmiljøer
   },
 };
 
+
+// Opretter forbindelse
 const connection = new Connection(config);
 
+
+// Event-lytter til 'connect' - dette udløses, når der oprettes forbindelse til databasen
 connection.on('connect', (err) => {
   if (err) {
-    console.error('Fejl ved forbindelse til Azure SQL Database:', err.message);
+    console.error('Fejl ved forbindelse til Azure SQL Database:', err);
   } else {
     console.log('Forbundet til Azure SQL Database');
 
@@ -85,26 +39,42 @@ connection.on('connect', (err) => {
   }
 });
 
+
+// Funktion til at udføre en SQL-forespørgsel (test af forbindelse til databasen)
 function executeStatement() {
   const request = new Request('SELECT * FROM Users', (err, rowCount) => {
     if (err) {
-      console.error('Fejl ved udførelse af SQL-forespørgsel:', err.message);
+      console.error('Fejl ved udførelse af SQL-forespørgsel:', err);
     } else {
       console.log(`${rowCount} rækker returneret`);
     }
 
-    connection.close();
+    connection.close(); // Luk forbindelsen, når operationen er afsluttet
   });
 
+  // Event-lytter til 'row' - dette udløses, når en række returneres fra databasen
   request.on('row', (columns) => {
     columns.forEach((column) => {
       console.log(`${column.metadata.colName}: ${column.value}`);
     });
   });
 
+  // Udfør SQL-forespørgslen
   connection.execSql(request);
 }
 
 
+// Event-lytter til 'end' som udløses, når forbindelsen afsluttes
+connection.on('end', () => {
+  console.log('Forbindelsen er afsluttet');
+});
 
-//database
+
+// Til at håndtere fejl ved forbindelse
+connection.on('error', (err) => {
+  console.error('Forbindelsesfejl:', err);
+});
+
+
+// Til at åbne forbindelsen
+connection.connect();
