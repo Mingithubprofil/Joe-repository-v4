82e7d1fdf8f621518f25dbcc7593398afd2066ba
Mingithubprofil <sikj21ad@student.cbs.dk>
2023-11-12 19:08:14 +0100

    files

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000
 
+
+app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
