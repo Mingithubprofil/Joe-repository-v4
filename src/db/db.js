451e8494e957733f4c2d2b59ef2001f71241cf07
Mingithubprofil <sikj21ad@student.cbs.dk>
2023-11-12 16:39:08 +0100

    db og routes

--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,45 @@
 
 
+const { Connection, Request } = require('tedious');
+
+const config = {
+  server: 'servertilhjemmeside.database.windows.net',
+  authentication: {
+    type: 'default',
+    options: {
+      userName: 'admin20',
+      password: 'Kodetdatabase20',
+    },
+  },
+  options: {
+    encrypt: true,
+    database: 'hjemmesidedatabase',
+    trustServerCertificate: false,
+  },
+};
+
+const connection = new Connection(config);
+
+// Forbind til SQL-databasen
+connection.connect((err) => {
+  if (err) {
+    console.error('Fejl ved forbindelse til SQL-database:', err.message);
+  } else {
+    console.log('Forbundet til SQL-database');
+  }
+});
+
+module.exports = connection;
+
+
+
+
+
+
+
+
+/*
+
 //database
 
 const { Connection, Request } = require('tedious');
@@ -40,6 +80,8 @@ connection.on('connect', (err) => {
 });
 
 
+/*
+
 // Funktion til at udføre en SQL-forespørgsel (test af forbindelse til databasen)
 function executeStatement() {
   const request = new Request('SELECT * FROM Users', (err, rowCount) => {
@@ -64,6 +106,47 @@ function executeStatement() {
 }
 
 
+
+
+
+function executeStatement(sql, parameters) {
+  return new Promise((resolve, reject) => {
+    const request = new Request(sql, (err, rowCount) => {
+      if (err) {
+        console.error('Fejl ved udførelse af SQL-forespørgsel:', err);
+        reject(err);
+      } else {
+        console.log(`${rowCount} rækker returneret`);
+        resolve(); // Opfyld løftet, når operationen er afsluttet uden fejl
+      }
+    });
+
+    // Tilknyt parameterne til forespørgslen, hvis de er defineret
+    if (parameters && parameters.length > 0) {
+      parameters.forEach((param) => {
+        request.addParameter(param.name, param.type, param.value);
+      });
+    }
+
+    // Event-lytter til 'row' - dette udløses, når en række returneres fra databasen
+    request.on('row', (columns) => {
+      columns.forEach((column) => {
+        console.log(`${column.metadata.colName}: ${column.value}`);
+      });
+    });
+
+    // Tilføj event-lytter til 'doneInProc', så vi kan håndtere slutningen af en forespørgsel med flere resultater
+    request.on('doneInProc', () => {
+      resolve(); // Opfyld løftet, når operationen er afsluttet uden fejl
+    });
+
+    // Udfør SQL-forespørgslen
+    connection.execSql(request);
+  });
+}
+
+
+
 // Event-lytter til 'end' som udløses, når forbindelsen afsluttes
 connection.on('end', () => {
   console.log('Forbindelsen er afsluttet');
@@ -78,3 +161,6 @@ connection.on('error', (err) => {
 
 // Til at åbne forbindelsen
 connection.connect();
+
+
+*/
