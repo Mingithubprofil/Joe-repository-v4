67399414647940b77647a9585af64aaabdf93ea4
Mingithubprofil <sikj21ad@student.cbs.dk>
2023-11-12 01:27:37 +0100

    files

--- src/db/db.js
+++ src/db/db.js
@@ -12,6 +12,33 @@ const config = {
   },
 };
 
+
+async function connectAndQuery() {
+    try {
+      await sql.connect(config);
+      console.log('Forbundet til Azure SQL Database');
+  
+      // Udfør SQL-operationer her (indsæt, opdater, hent, slet data)
+      const result = await sql.request().query('SELECT * FROM Users');
+      console.log('Data fra databasen:', result.recordset);
+    } catch (err) {
+      console.error('Fejl ved forbindelse til Azure SQL Database:', err);
+      console.error('Fejlbesked:', err.message); // Tilføj fejlmeddelelsen til logningen
+      console.error('Fejldetaljer:', err);
+    } finally {
+      // Luk forbindelsen ved afslutning af din applikation
+      sql.close();
+      console.log('Forbindelse til Azure SQL Database lukket ved afslutning.');
+    }
+  }
+  
+  // Kald funktionen for at starte processen
+  connectAndQuery();
+  
+
+
+/*
+
 sql.connect(config)
   .then(pool => {
     console.log('Forbundet til Azure SQL Database');
@@ -32,6 +59,8 @@ sql.connect(config)
     //sql.close();
     console.log('Forbindelse til Azure SQL Database lukket ved afslutning.');
   });
+
+  */
 
 
 /* 
