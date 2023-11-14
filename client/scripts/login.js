ab8313b34f44a60477c5ffc5333becf1f6b8e10a
Mingithubprofil <sikj21ad@student.cbs.dk>
2023-11-14 23:21:35 +0100

    routes

--- client/scripts/login.js
+++ client/scripts/login.js
@@ -50,7 +50,7 @@ function loginUser() {
     headers: {
       'Content-Type': 'application/json',
     },
-    timeout: 10000, // Øg timeout til 10 sekunder (eller mere efter behov)
+    timeout: 20000, // Øg timeout til 10 sekunder (eller mere efter behov)
   })
     .then(async function (response) {
       if (response.data.userExists) {
@@ -76,7 +76,7 @@ async function performLogin(username, password) {
       headers: {
         'Content-Type': 'application/json',
       },
-      timeout: 10000, 
+      timeout: 20000, 
     });
 
     console.log(response.data);
