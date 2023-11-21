02580e5d585e8bda313c1078a354ba00fff8b260
Mingithubprofil <sikj21ad@student.cbs.dk>
2023-11-21 15:45:13 +0100

    routes

--- client/scripts/userHome.js
+++ client/scripts/userHome.js
@@ -4,6 +4,12 @@ let username = getCookie("userAuth");
 document.querySelector("username_on_page").innerHTML = `Welcome ${username}!`
 if (!username) location.href = "/login";
 
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  }
+
 
 
 
