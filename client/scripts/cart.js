9b4d6220a7134a34cf0c6caaf3f1f9eb8ef171ed
AlfredKhan <alkh22ab@student.cbs.dk>
2023-11-22 21:05:28 +0100

    sms

--- client/scripts/cart.js
+++ client/scripts/cart.js
@@ -1,32 +1,26 @@
-
-  window.addEventListener('DOMContentLoaded', (event) => {
-    openLocationPopup();
+window.addEventListener('DOMContentLoaded', (event) => {
+  openLocationPopup();
 });
 
 function openLocationPopup() {
-    document.getElementById('locationPopup').style.display = 'flex';
+  document.getElementById('locationPopup').style.display = 'flex';
 }
 
 function closeLocationPopup() {
-    document.getElementById('locationPopup').style.display = 'none';
+  document.getElementById('locationPopup').style.display = 'none';
 }
 
 function selectCity() {
-    var selectedCity = document.getElementById('cities').value;
-    // Handle the selected city as needed (e.g., pass it to your server, store in a variable, etc.)
-    console.log("Selected City:", selectedCity);
+  var selectedCity = document.getElementById('cities').value;
+  console.log("Selected City:", selectedCity);
 
-    // Update the text with the selected city
-    var orderTextElement = document.getElementById('orderText');
-    if (orderTextElement) {
-        orderTextElement.innerText = `På denne side kan du bestille en juice fra vores lækre menu i ${selectedCity}!`;
-    }
+  var orderTextElement = document.getElementById('orderText');
+  if (orderTextElement) {
+    orderTextElement.innerText = `På denne side kan du bestille en juice fra vores lækre menu i ${selectedCity}!`;
+  }
 
-    // Close the popup (you can remove this if you want to keep it open)
-    closeLocationPopup();
+  closeLocationPopup();
 }
-
-
 
 document.addEventListener('DOMContentLoaded', function () {
   const addToCartButtons = document.querySelectorAll('.addToCartBtn');
@@ -34,121 +28,132 @@ document.addEventListener('DOMContentLoaded', function () {
   const closeBtn = document.querySelector('.close');
   const cartItemsList = document.getElementById('cartItems');
   const totalPriceElement = document.getElementById('totalPrice');
-  const cartIcon = document.getElementById('cartIcon'); // Cart icon ID
-  const betalBtn = document.querySelector('.betalBtn'); // Betal knap
+  const cartIcon = document.getElementById('cartIcon');
+  const betalBtn = document.querySelector('.betalBtn');
+  const paymentTotalElement = document.getElementById('paymentTotal');
+  const submitPaymentBtn = document.getElementById('Betalnu');
 
   let cartItems = [];
 
-  // Event listeners for "Add to Cart" buttons
   addToCartButtons.forEach(button => {
-      button.addEventListener('click', () => addToCart(button));
+    button.addEventListener('click', () => addToCart(button));
   });
 
-  // Event listener for opening the cart modal when clicking the cart icon
   cartIcon.addEventListener('click', openCart);
-
-  // Event listener for closing the cart modal
   closeBtn.addEventListener('click', closeCart);
-
-  // Event listener for opening the payment form
   betalBtn.addEventListener('click', openPaymentForm);
 
-  // Function to add an item to the cart
+  submitPaymentBtn.addEventListener('click', submitPayment);
+
   function addToCart(button) {
-      const productContainer = button.closest('.product');
-      const productName = productContainer.querySelector('h3').textContent;
+    const productContainer = button.closest('.product');
+    const productName = productContainer.querySelector('h3').textContent;
 
-      // Find the price element and extract the numerical value
-      const priceElement = productContainer.querySelector('p[pricetag]');
-      const productPrice = parseFloat(priceElement.getAttribute('pricetag'));
+    const priceElement = productContainer.querySelector('p[pricetag]');
+    const productPrice = parseFloat(priceElement.getAttribute('pricetag'));
 
-      const cartItem = { name: productName, price: productPrice };
+    const cartItem = { name: productName, price: productPrice };
 
-      cartItems.push(cartItem);
-      updateCartView();
+    cartItems.push(cartItem);
+    updateCartView();
   }
 
-  // Function to remove an item from the cart
   function removeFromCart(index) {
-      cartItems.splice(index, 1);
-      updateCartView();
+    cartItems.splice(index, 1);
+    updateCartView();
   }
 
-  // Function to update the cart view
   function updateCartView() {
-      cartItemsList.innerHTML = '';
-      let total = 0;
+    cartItemsList.innerHTML = '';
+    let total = 0;
 
-      cartItems.forEach((item, index) => {
-          const listItem = document.createElement('li');
-          listItem.innerHTML = `
-              ${item.name} - ${item.price} kr.
-              <button class="removeBtn" data-index="${index}">Remove</button>
-          `;
-          cartItemsList.appendChild(listItem);
+    cartItems.forEach((item, index) => {
+      const listItem = document.createElement('li');
+      listItem.innerHTML = `
+        ${item.name} - ${item.price} kr.
+        <button class="removeBtn" data-index="${index}">Remove</button>
+      `;
+      cartItemsList.appendChild(listItem);
 
-          // Add event listener to the remove button
-          const removeBtn = listItem.querySelector('.removeBtn');
-          removeBtn.addEventListener('click', () => removeFromCart(index));
+      const removeBtn = listItem.querySelector('.removeBtn');
+      removeBtn.addEventListener('click', () => removeFromCart(index));
 
-          total += item.price;
-      });
+      total += item.price;
+    });
 
-      totalPriceElement.textContent = `Total: ${total.toFixed(2)} kr.`;
+    totalPriceElement.textContent = `Total: ${total.toFixed(2)} kr.`;
 
-      openCart();
+    updatePaymentTotal(total);
+
+    openCart();
   }
 
-  // Function to open the cart modal
   function openCart() {
-      cartModal.style.width = '250px'; // Set the width as desired
+    cartModal.style.width = '250px';
   }
 
-  // Function to close the cart modal
   function closeCart() {
-      cartModal.style.width = '0';
+    cartModal.style.width = '0';
   }
 
-  // Function to open the payment form
   function openPaymentForm() {
-    // Close the cart modal
     closeCart();
-
-    // Set the display property of the payment form to 'block'
     document.getElementById('paymentForm').style.display = 'block';
-}
+  }
+
+  function updatePaymentTotal(total) {
+    if (paymentTotalElement) {
+      paymentTotalElement.textContent = `Total beløb: ${total.toFixed(2)} kr.`;
+    }
+  }
 });
 
 
+function submitPayment() {
+  const accountSid = 'AC12cb9761bd22a85b3994135bbcc68e65';
+  const authToken = 'bdbc23653559799ad052af1277f0091a';
+  const client = new Twilio(accountSid, authToken);
 
+  const name = document.getElementById("name").value;
+  const telefonnummer = document.getElementById("telefonnummer").value;
 
+  client.messages
+    .create({
+      body: `Hej ${name}, det er JOE. I dag er der tilbud på Juicen. Kom ind i vores cafe og prøv den.`,
+      messagingServiceSid: 'MG178da6c222de9ec03486b61a2e72c85e',
+      to: telefonnummer
+    })
+    .then(message => {
+      console.log(message);
+      showOrderConfirmation();
+    });
+}
 
-
-
-// Function to show order confirmation
 function showOrderConfirmation() {
-  // Get the selected location from the dropdown
   document.getElementById('paymentForm').style.display = 'none';
   document.getElementById('orderConfirmation').style.display = 'block';
   const selectedLocation = document.getElementById("cities").value;
 
-  // Update the content of the order confirmation popup
   const orderConfirmation = document.getElementById("orderConfirmation");
   if (orderConfirmation) {
-    // Find the span element inside the popup
     const selectedLocationElement = orderConfirmation.querySelector("#selectedLocation");
 
-    // Update the text with the selected location
     if (selectedLocationElement) {
       selectedLocationElement.innerText = selectedLocation;
     }
 
-    // Show order confirmation popup
     orderConfirmation.style.display = "block";
   }
 }
 
-// Function to hide order confirmation popup
 function hideOrderConfirmation() {
   document.getElementById("orderConfirmation").style.display = "none";
+}
+
+function closepaymentPopup() {
+  document.getElementById('paymentForm').style.display = 'none';
+}
+
+function closeorderConfirmation() {
+  document.getElementById('orderConfirmation').style.display = 'none';
 }
