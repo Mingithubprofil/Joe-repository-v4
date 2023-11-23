10cd951d3a3e5178b842381e12e6300c21865ff3
AlfredKhan <alkh22ab@student.cbs.dk>
2023-11-23 11:56:08 +0100

    sms

--- client/scripts/cart.js
+++ client/scripts/cart.js
@@ -108,20 +108,19 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 
-
 function submitPayment() {
   const accountSid = 'AC12cb9761bd22a85b3994135bbcc68e65';
-  const authToken = 'bdbc23653559799ad052af1277f0091a';
-  const client = new Twilio(accountSid, authToken);
+  const authToken = 'a762494ae79bad3c353db3fcd9b840f0';
+  const client = require('twilio')(accountSid, authToken);
 
   const name = document.getElementById("name").value;
   const telefonnummer = document.getElementById("telefonnummer").value;
 
   client.messages
     .create({
-      body: `Hej ${name}, det er JOE. I dag er der tilbud på Juicen. Kom ind i vores cafe og prøv den.`,
+      body: `Hej ${name}, din ordre er nu modtaget og vil blive leveret indenfor 5 minutter. Tak fordi du valgte `,
       messagingServiceSid: 'MG178da6c222de9ec03486b61a2e72c85e',
-      to: telefonnummer
+      to: `+45${telefonnummer}`
     })
     .then(message => {
       console.log(message);
