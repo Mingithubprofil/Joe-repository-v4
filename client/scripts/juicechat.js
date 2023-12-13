cc35a48d75a4da6f81ad8503c7d8da5debdd66e3
Mingithubprofil <sikj21ad@student.cbs.dk>
2023-12-13 21:07:45 +0100

    kommentarer

--- client/scripts/juicechat.js
+++ client/scripts/juicechat.js
@@ -1,17 +1,11 @@
+
+
+//Implementering af socket.io til juicechatten
+
 const socket = io();
 
 const messages = document.getElementById("messages");
 const input = document.getElementById("input");
-
-
-// form.addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   if (input.value) {
-//     socket.emit('chat message', input.value);
-//     input.value = '';
-//   }
-// });
-
 
 function sendChat() {
   if (input.value) {
@@ -20,51 +14,26 @@ function sendChat() {
   }
 }
 
-
 socket.on("chat message", (msg) => {
   const item = document.createElement("li");
   item.textContent = msg;
   messages.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
 });
 
-
-// Cookie her:
+// Her tjekkes om brugeren er logget ind, hvis ikke så dirigeres brugeren til login-siden:
 
 let username = getCookie("userAuth");
 if (!username) location.href = "/login";
 
-
 socket.emit("user joined", username);
 
+// Her hentes brugerens cookie
 
 function getCookie(name) {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop().split(";").shift();
 }
-
-/*
-//visning af username på siden
-
-if (username) {
-  
-  const elements = document.querySelectorAll(".username_on_page");
-
-  elements.forEach(element => {
-    element.innerHTML = username;
-  });
-} */
-
-
-
-
-
-
-
-
-
-
 
 
 
